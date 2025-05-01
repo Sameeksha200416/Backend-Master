86528863aa15d8de3d0cf4624ec503d3844c74ef
Sameeksha200416 <sameekshavishwakarma2004@gmail.com>
2025-05-01 17:52:59 +0530

    debigging

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js"
 
 
 dotenv.config({
@@ -11,7 +12,7 @@ connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 8000, () => {
         console.log(`server is running at port :
-            ${process.env.PORT}`);
+             ${process.env.PORT}`);
     })
 })
 .catch((err) => {
